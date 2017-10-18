142300101f5b1f31fc3a5655480353d77e1c611a
Naukkis <22678066+Naukkis@users.noreply.github.com>
2017-10-18 15:51:27 +0300

    Initial commit

---
+++ leds.py
@@ -0,0 +1,41 @@
+import datetime
+from subprocess import call, check_output
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d-%H-%M-%S")
+    call(["fswebcam", "-r", "640x480", "/home/ville/ledit/static/"+ timeString + ".jpg"])
+    imageName = "/static/" + timeString + ".jpg"
+
+    led1 = check_output(["gpio", "-g", "read", "17"], universal_newlines=True).rstrip()
+    led2 = check_output(["gpio", "-g", "read", "22"], universal_newlines=True).rstrip()
+    led3 = check_output(["gpio", "-g", "read", "18"], universal_newlines=True).rstrip()
+    led4 = check_output(["gpio", "-g", "read", "23"], universal_newlines=True).rstrip()
+
+    templateData = {
+        'led1status': led1,
+        'led2status': led2,
+        'led3status': led3,
+        'led4status': led4,
+        'image': imageName
+    }
+    return render_template('index.html', **templateData)
+
+@app.route('/ledToggle')
+def ledToggle():
+    a = request.args.get('a', 0)
+    b = request.args.get('b', 0)
+
+    if a == "ON":
+        call(["gpio", "-g", "write", b, "1"])
+    else:
+        call(["gpio", "-g", "write", b, "0"])
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    pass
